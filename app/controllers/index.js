c0da388f7b3d6f50a8a48f61a1f35fb33de632a2
Erika Pauwels <erika.pauwels@gmail.com>
2022-05-19 00:47:07 +0200

    Add route listing events and containing form to create new event

---
+++ app/controllers/index.js
@@ -0,0 +1,45 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { isPresent } from '@ember/utils';
+
+export default class IndexController extends Controller {
+  @service store;
+  @service router;
+
+  @tracked name;
+  @tracked date;
+  @tracked time;
+
+  get isValid() {
+    return isPresent(this.name) && isPresent(this.date) && isPresent(this.time);
+  }
+
+  @action
+  async createEvent(e) {
+    e.preventDefault();
+    const event = this.store.createRecord('event', {
+      name: this.name,
+      startDate: new Date(Date.parse(`${this.date}T${this.time}:00`)),
+    });
+    await event.save();
+
+    this.router.refresh();
+  }
+
+  @action
+  setEventName(e) {
+    this.name = e.target.value;
+  }
+
+  @action
+  setEventDate(e) {
+    this.date = e.target.value;
+  }
+
+  @action
+  setEventTime(e) {
+    this.time = e.target.value;
+  }
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,25 @@ import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service session;
+  @service store;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+  }
+
+  model() {
+    return this.store.query('event', {
+      sort: '-start-date',
+      page: {
+        size: 100,
+      },
+    });
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.name = 'Repetitie';
+    controller.date = new Date().toISOString().substr(0, 'YYYY-MM-DD'.length);
+    controller.time = '20:00';
   }
 }
