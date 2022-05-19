2585b0597b469a331e2bac636539064cb6435824
Erika Pauwels <erika.pauwels@gmail.com>
2022-05-19 02:16:30 +0200

    Move musicians to separate route

---
+++ app/routes/people.js
@@ -0,0 +1,20 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class PeopleRoute extends Route {
+  @service session;
+  @service store;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+
+  model() {
+    return this.store.query('person', {
+      sort: 'family-name,given-name',
+      page: {
+        size: 100,
+      },
+    });
+  }
+}
