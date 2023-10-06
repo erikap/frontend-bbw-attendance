fa683cde150651cd9cd511ec27873633b430dc7c
Erika Pauwels <erika.pauwels@gmail.com>
2023-10-06 11:49:55 +0200

    Bump ember-simple-auth and ember-mu-login

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
   @service router;
+  @service session;
+
+  async beforeModel() {
+    await this.session.setup();
+  }
 
   activate() {
     this.router.on('routeWillChange', () => {
